5a55c4e1f00600f8c13dd229125345895aa6e05c
Pixel <scott.blatt.0b10@gmail.com>
2023-10-30 14:28:26 -0500

    FEAT: Add shell defaults

---
+++ config/files/usr/etc/profile.d/.theme.bash
@@ -0,0 +1,16 @@
+# Data
+USERNAME="\u"
+CURRENT_PATH="\[\w\]"
+# Ensure colors are set
+source /etc/profile.d/theme-colors.sh
+# Conditional prompt values
+if [[ $UID -eq 0 ]];then
+	COL_PROMPT="${COL_RED}"
+	PROMPT_CHAR="#"
+else
+	COL_PROMPT="${COL_GREEN}"
+	PROMPT_CHAR="$"
+fi
+
+export PS1="\n${COL_RESET}┌──(${COL_PROMPT}${USERNAME}${COL_RESET}@${COL_BLUE}${HOSTNAME}${COL_RESET})-[${COL_CYAN}${CURRENT_PATH}${COL_RESET}]\n└─${COL_PROMPT}${PROMPT_CHAR}${COL_RESET} "
+export PS2="${COL_PROMPT}${PROMPT_CHAR}${COL_RESET}>> "
